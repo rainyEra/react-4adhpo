47be699006fb70c8e3d5ec61d7cc65eaa40b03b5
rainyEra <79323963+rainyEra@users.noreply.github.com>
2022-10-07 11:26:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4adhpo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4adhpo)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './style.css';
+import styling from './badon.module.css';
+export default function App() {
+  return (
+    <div className={styling.zigi}>
+      <div className={styling.zigus}>
+        <h1>Zigi</h1>
+      </div>
+    </div>
+  );
+}
